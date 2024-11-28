555eea2cf1bc3b06e0024c8e7ad71c15288a2959
Amir3158 <amir.kaya7@gmail.com>
2024-11-28 12:09:29 +0200

    update and added necessary files

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,13 @@
-import axios from "axios";
+import express from 'express';
+import router from "./route";
 
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT,
+    () => console.log(`Server is running on port ${PORT}`)
+);
