e0d276b240723f36475a2249b25d776ace44414b
Amir3158 <amir.kaya7@gmail.com>
2024-12-02 12:44:44 +0200

    minor changes

--- src/service/octroPrintWrapper.service.ts
+++ src/service/octroPrintWrapper.service.ts
@@ -1,11 +1,9 @@
 import {sendOctoPrintPostRequest} from "../utils/octoPrint.utils";
 
-export const sendWarmUpBedTempRequest = (target : number) => {
+export const sendWarmUpBedTempRequest = async (target : number) => {
     const options = {
         "command": "target",
         "target": target
     }
     const response = await sendOctoPrintPostRequest('http://localhost:5000/api/printer/bed', options);
-
-
 }
--- src/utils/octoPrint.utils.ts
+++ src/utils/octoPrint.utils.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 
 
-export const sendOctoPrintPostRequest = async (path : String, options : any) => {
+export const sendOctoPrintPostRequest = async (path : string, options : any) => {
     // Make the POST request
+    const octoPrintApiKey = process.env.OCTOPRINT_API_KEY || "";
     try {
-        const response = await axios.post(path, options, {
+        return await axios.post(path, options, {
             headers: {
                 "Content-Type": "application/json",
-                "X-Api-Key": "", // API Key for authentication
+                "X-Api-Key": octoPrintApiKey, // API Key for authentication
             },
         });
-    } catch (error) {
+    } catch (error: any) {
+        console.error(" failed to warm the bed", error.message);
+        throw error;
     }
 }
 
